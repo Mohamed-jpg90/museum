4a6fb56e8f159dbb53d59bdfdacfd369f648cd1c
Mohamed Maged <149176668+Mohamed-jpg90@users.noreply.github.com>
2024-08-16 15:23:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
+
+// var x =prompt('enter the numbre of students')
+// for(let i=0 ;i<=x ;i++)
+// {
+//    console.log('enter the degre of the stydent ')
+// }
+
+
+
+
+
+// script.js
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', () => {
+  alert('your mesage has been sent ');
+
+});
+const myButton1 = document.getElementById('myButton1');
+
+myButton1.addEventListener('click', () => {
+  alert('your mesage has been sent ');
+
+});
+const myButton2 = document.getElementById('myButton2');
+
+myButton2.addEventListener('click', () => {
+  alert('your mesage has been sent ');
+
+});
